a4fbb4f755126ac7505d63ad2aee8b7e2cdfb6a3
DSPAUL <paul.de.smul@gmail.com>
2020-02-06 00:29:56 +0100

    Added New Files

---
+++ COMPASS/Tools/FilterHandler.cs
@@ -0,0 +1,117 @@
+﻿using COMPASS.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMPASS.Tools
+{
+    public class FilterHandler : ObservableObject
+    {
+        readonly Data data;
+
+        //Constuctor
+        public FilterHandler(Data CurrentData)
+        {
+            data = CurrentData;
+            SearchTerm = "";
+            TagFilteredFiles = new ObservableCollection<MyFile>(data.AllFiles);
+            SearchFilteredFiles = new ObservableCollection<MyFile>(data.AllFiles);
+            ActiveFiles = new ObservableCollection<MyFile>(data.AllFiles);
+            ActiveTags = new ObservableCollection<Tag>();
+        }
+
+        //Collections
+        public ObservableCollection<Tag> ActiveTags { get; set; }
+
+        public ObservableCollection<MyFile> TagFilteredFiles { get; set; }
+        public ObservableCollection<MyFile> SearchFilteredFiles { get; set; }
+        public ObservableCollection<MyFile> ActiveFiles { get; set; }
+
+        //Properties
+        private string searchterm;
+        public string SearchTerm
+        {
+            get { return searchterm; }
+            set 
+            { 
+                SetProperty(ref searchterm, value);
+
+                SearchFilteredFiles = new ObservableCollection<MyFile>(data.AllFiles);
+
+                if(searchterm != "")
+                {
+                    foreach (MyFile f in data.AllFiles)
+                    {
+                        if (f.Title.IndexOf(SearchTerm, StringComparison.OrdinalIgnoreCase) < 0) SearchFilteredFiles.Remove(f);
+                    }
+                }
+                Update_ActiveFiles();
+            }
+        }       
+
+        //Funcitons
+        public void ClearFilters()
+        {
+            SearchTerm = "";
+
+            TagFilteredFiles = new ObservableCollection<MyFile>(data.AllFiles);
+            SearchFilteredFiles = new ObservableCollection<MyFile>(data.AllFiles);
+            ActiveFiles = new ObservableCollection<MyFile>(data.AllFiles);
+            ActiveTags = new ObservableCollection<Tag>();
+        }
+
+        public void AddTagFilter(Tag t)
+        {
+            if (ActiveTags.All(p => p.ID != t.ID))
+            {
+                ActiveTags.Add(t);
+            }
+            foreach (MyFile f in data.AllFiles)
+            {
+                if (!f.Tags.Contains(t) && TagFilteredFiles.Contains(f)) TagFilteredFiles.Remove(f);
+            }
+            Update_ActiveFiles();
+        }
+
+        public void RemoveTagFilter(Tag t)
+        {
+            ActiveTags.Remove(t);
+            TagFilteredFiles.Clear();
+            foreach (MyFile f in data.AllFiles)
+            {
+                if (ActiveTags.All(i => f.Tags.Contains(i)))
+                    TagFilteredFiles.Add(f);
+            }
+            Update_ActiveFiles();
+        }
+
+        public void Update_ActiveFiles()
+        {
+            if (ActiveFiles == null) return;
+
+            //if (SearchFilteredFiles.Count == data.AllTags.Count) 
+            //{
+            //    ActiveFiles = new ObservableCollection<MyFile>(TagFilteredFiles);
+            //    return;
+            //}
+            //if (TagFilteredFiles.Count == data.AllTags.Count)
+            //{
+            //    ActiveFiles = new ObservableCollection<MyFile>(TagFilteredFiles);
+            //    return;
+            //}
+            ActiveFiles.Clear();
+            foreach (var p in TagFilteredFiles.Intersect(SearchFilteredFiles))
+                ActiveFiles.Add(p);
+        }
+
+        public void RemoveFile(MyFile f)
+        {
+            TagFilteredFiles.Remove(f);
+            SearchFilteredFiles.Remove(f);
+            Update_ActiveFiles();
+        }
+    }
+}
---
+++ COMPASS/ViewModels/Commands/SimpleCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace COMPASS.ViewModels.Commands
+{
+    public class SimpleCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        readonly Action<Object> _execute;
+
+        public SimpleCommand(Action<Object> Execute)
+        {
+            _execute = Execute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute.Invoke(parameter);
+        }
+    }
+}
