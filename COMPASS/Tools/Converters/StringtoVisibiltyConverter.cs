b77083c0b1e5e412bde5d76c9dfe70f6d0e92a6f
DSPAUL <paul.de.smul@gmail.com>
2020-02-08 16:27:33 +0100

    Use ValueConverter for WindowState

--- COMPASS/Tools/Converters/StringtoVisibiltyConverter.cs
+++ COMPASS/Tools/Converters/StringtoVisibiltyConverter.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Data;
 
-namespace COMPASS.Tools
+namespace COMPASS.Tools.Converters
 {
     public class StringtoVisibiltyConverter : IValueConverter
     {
