0d778cdb4099d9e75c2f33b4d2236c049465cf63
DSPAUL <paul.de.smul@gmail.com>
2020-02-22 11:43:23 +0100

    Finished Progress Bar

--- COMPASS/ViewModels/ImportViewModel.cs
+++ COMPASS/ViewModels/ImportViewModel.cs
@@ -61,6 +61,14 @@ namespace COMPASS.ViewModels
             set { SetProperty(ref _progress, value); }
         }
 
+        private readonly string _importText = "Import in Progress: \n {0} out of {1}";
+        private int _importcounter;
+        private int _importamount;
+        public string ImportText
+        {
+            get { return string.Format(_importText,_importcounter,_importamount); }
+        }
+
         #endregion
 
         #region Functions and Events
@@ -72,6 +80,7 @@ namespace COMPASS.ViewModels
                 AddExtension = false,
                 Multiselect = true
             };
+
             if (openFileDialog.ShowDialog() == true)
             {
                 ProgressWindow pgw;
@@ -82,13 +91,13 @@ namespace COMPASS.ViewModels
                         pgw = new ProgressWindow(this);
                         pgw.Show();
                     });
-                int prog_counter = 1;
+                int progcounter = 1;
                 foreach (string path in openFileDialog.FileNames)
                 {
                     //Update Progress Bar
-                    float prog = (float)prog_counter / openFileDialog.FileNames.Length *100;
-                    prog_counter++;
-                    (sender as BackgroundWorker).ReportProgress((int)prog);
+                    float prog = (float)progcounter / openFileDialog.FileNames.Length  * 100;               
+                    (sender as BackgroundWorker).ReportProgress((int)prog, new Tuple<int, int>(progcounter, openFileDialog.FileNames.Length));
+                    progcounter++;
 
                     //Import File
                     if (_data.AllFiles.All(p => p.Path != path))
@@ -97,7 +106,6 @@ namespace COMPASS.ViewModels
                         _data.AllFiles.Add(pdf);
                         CoverArtGenerator.ConvertPDF(pdf, _data.Folder);
                     }
-                    Thread.Sleep(1000);
                 }
             }
             Application.Current.Dispatcher.Invoke(() =>
@@ -108,16 +116,16 @@ namespace COMPASS.ViewModels
 
         void ImportManual(object sender, DoWorkEventArgs e)
         {
-            for (int i = 0; i < 100; i++)
-            {
-                (sender as BackgroundWorker).ReportProgress(i);
-                Thread.Sleep(100);
-            }
+            
         }
 
         private void ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
             Progress = e.ProgressPercentage;
+            var args = (Tuple<int, int>)e.UserState;
+            _importcounter = args.Item1;
+            _importamount = args.Item2;
+            RaisePropertyChanged("ImportText");
         }
 
         #endregion
