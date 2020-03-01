542fc2706d9594af38d4aaccabc471b122c8f5b8
DSPAUL <paul.de.smul@gmail.com>
2020-03-01 15:30:37 +0100

    Fixed namespace tag and file

--- COMPASS/Models/MyFile.cs
+++ COMPASS/Models/MyFile.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
-namespace COMPASS
+namespace COMPASS.Models
 {
     public class MyFile : ObservableObject
     {
