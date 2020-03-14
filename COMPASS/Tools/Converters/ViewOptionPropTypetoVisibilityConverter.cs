37cc63e22d5a6c0bcb8e8494b39efc718c732f4d
DSPAUL <paul.de.smul@gmail.com>
2020-03-14 10:20:43 +0100

    Added Converter
    Only show relevant control for viewoption type, e.g. checkbox for bool, slider for float

---
+++ COMPASS/Tools/Converters/ViewOptionPropTypetoVisibilityConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace COMPASS.Tools.Converters
+{
+    public class ViewOptionPropTypetoVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return null;
+            switch (parameter)
+            {
+                case "Boolean":
+                    if (value.GetType() == typeof(bool)) return Visibility.Visible;
+                    break;
+
+                case "Float":
+                    if (value.GetType() == typeof(float)) return Visibility.Visible;
+                    break;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
