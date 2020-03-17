6d7253d8b807a2322185b1fee6d6932b1b1ff573
DSPAUL <paul.de.smul@gmail.com>
2020-03-17 16:39:31 +0100

    Addes cover size slider and Title toggle

--- COMPASS/ViewModels/FileTileViewModel.cs
+++ COMPASS/ViewModels/FileTileViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using COMPASS.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +12,34 @@ namespace COMPASS.ViewModels
     {
         public FileTileViewModel(MainViewModel vm = null) : base(vm)
         {
-
+            ViewOptions = new ObservableCollection<MyMenuItem>();
+            ViewOptions.Add(new MyMenuItem("Cover Size", value => TileWidth = (double)value) { Prop = TileWidth });
+            ViewOptions.Add(new MyMenuItem("Show Title", value => ShowTitle = (bool)value) { Prop = ShowTitle });
         }
+        #region Properties
+        private double _width = 156;
+        public double TileWidth
+        {
+            get { return _width; }
+            set 
+            { 
+                SetProperty(ref _width, value);
+                RaisePropertyChanged(nameof(TileHeight));
+            }
+        }
+
+        public double TileHeight
+        {
+            get { return (int)(_width * 4/3); }
+        }
+
+        private bool _showtitle = false;
+        public bool ShowTitle
+        {
+            get { return _showtitle; }
+            set { SetProperty(ref _showtitle, value); }
+        }
+
+        #endregion
     }
 }
