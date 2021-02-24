2af11db591c0c93db4f6aa0f7456e55e391a161c
DSPAUL <paul.de.smul@gmail.com>
2021-02-24 23:15:04 +0100

    Design tweaks

--- COMPASS/Resources/Controls/ExpandingButton.xaml.cs
+++ COMPASS/Resources/Controls/ExpandingButton.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -52,6 +53,83 @@ namespace COMPASS.Resources.Controls
         private void CheckedChanged(object sender, RoutedEventArgs e)
         {
             ToggleBtn.IsChecked = !ToggleBtn.IsChecked;
+
+            //if (HiddenContentGrid.Visibility == Visibility.Collapsed)
+            //{
+            //    HiddenContentGrid.Visibility = Visibility.Visible;
+            //    HiddenContentGrid.Measure(new Size(Double.PositiveInfinity, Double.PositiveInfinity));
+            //    DoubleAnimation animHeight = new DoubleAnimation();
+            //    DoubleAnimation animWidth = new DoubleAnimation();
+            //    animHeight.From = OutsidePanel.ActualHeight;
+            //    animWidth.From = OutsidePanel.ActualWidth;
+            //    animHeight.To = OutsidePanel.ActualHeight + HiddenContentGrid.DesiredSize.Height;
+            //    animWidth.To = Math.Max(OutsidePanel.ActualWidth, HiddenContentGrid.DesiredSize.Width);
+            //    animHeight.Duration = new Duration(TimeSpan.FromSeconds(0.25));
+            //    animWidth.Duration = new Duration(TimeSpan.FromSeconds(0.15));
+            //    Storyboard.SetTarget(animHeight, OutsidePanel);
+            //    Storyboard.SetTarget(animWidth, OutsidePanel);
+            //    Storyboard.SetTargetProperty(animHeight, new PropertyPath(HeightProperty));
+            //    Storyboard.SetTargetProperty(animWidth, new PropertyPath(WidthProperty));
+            //    Storyboard st = new Storyboard();
+            //    st.Children.Add(animHeight);
+            //    //st.Children.Add(animWidth);
+            //    st.Begin();
+            //}
+            //else
+            //{
+            //    HiddenContentGrid.Measure(new Size(Double.PositiveInfinity, Double.PositiveInfinity));
+            //    DoubleAnimation animHeight = new DoubleAnimation();
+            //    DoubleAnimation animWidth = new DoubleAnimation();
+            //    animHeight.From = OutsidePanel.ActualHeight;
+            //    animWidth.From = OutsidePanel.ActualWidth;
+            //    animHeight.To = OutsidePanel.ActualHeight - HiddenContentGrid.DesiredSize.Height;
+            //    //animWidth.To = ((ContentControl)ToggleBtn.Template.FindName("ToggleBtnContent", ToggleBtn)).Width;
+            //    //ToggleBtn.Measure(new Size(Double.PositiveInfinity, Double.PositiveInfinity));
+            //    var parent = VisualTreeHelper.GetParent(ExpandingBtnControl) as Grid;
+            //    parent.Measure(new Size(Double.PositiveInfinity, Double.PositiveInfinity));
+            //    animWidth.To = parent.DesiredSize.Width;
+            //    animHeight.Duration = new Duration(TimeSpan.FromSeconds(0.25));
+            //    animWidth.Duration = new Duration(TimeSpan.FromSeconds(0.15));
+            //    Storyboard.SetTarget(animHeight, OutsidePanel);
+            //    Storyboard.SetTarget(animWidth, OutsidePanel);
+            //    Storyboard.SetTargetProperty(animHeight, new PropertyPath(HeightProperty));
+            //    Storyboard.SetTargetProperty(animWidth, new PropertyPath(WidthProperty));
+            //    Storyboard st = new Storyboard();
+            //    st.Children.Add(animHeight);
+            //    //st.Children.Add(animWidth);
+            //    st.Completed += (a, b) => { 
+            //        HiddenContentGrid.Visibility = Visibility.Collapsed;
+            //    };
+            //    st.Begin();
+            //}
+        }
+
+        bool animating = false;
+
+        private void HiddenContentControl_LayoutUpdated(object sender, EventArgs e)
+        {
+            if (!animating)
+            {
+                animating = true;
+                HiddenContentGrid.Measure(new Size(1920, 700));
+                DoubleAnimation animHeight = new DoubleAnimation();
+                DoubleAnimation animWidth = new DoubleAnimation();
+                animHeight.From = OutsidePanel.ActualHeight;
+                animWidth.From = OutsidePanel.ActualWidth;
+                animHeight.To = HiddenContentGrid.DesiredSize.Height + ToggleBtn.ActualHeight;
+                animWidth.To = Math.Max(HiddenContentGrid.DesiredSize.Width, ToggleBtn.ActualWidth);
+                animHeight.Duration = new Duration(TimeSpan.FromSeconds(0.3));
+                animWidth.Duration = new Duration(TimeSpan.FromSeconds(0.3));
+                Storyboard.SetTarget(animHeight, OutsidePanel);
+                Storyboard.SetTarget(animWidth, OutsidePanel);
+                Storyboard.SetTargetProperty(animHeight, new PropertyPath(HeightProperty));
+                Storyboard.SetTargetProperty(animWidth, new PropertyPath(WidthProperty));
+                Storyboard st = new Storyboard();
+                st.Children.Add(animHeight);
+                //st.Children.Add(animWidth);
+                st.Completed += (a, b) => { animating = false; };
+                st.Begin();
+            }           
         }
     }
 }
