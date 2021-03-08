16bdb164707b7ad124b9a6a1b39839d55e346e9d
DSPAUL <paul.de.smul@gmail.com>
2021-03-09 00:07:41 +0100

    Save column visibility

--- COMPASS/ViewModels/FileListViewModel.cs
+++ COMPASS/ViewModels/FileListViewModel.cs
@@ -21,7 +21,7 @@ namespace COMPASS.ViewModels
                 Submenus = new ObservableCollection<MyMenuItem>()
             };
 
-            Columnvisibility.Submenus.Add(new MyMenuItem("Title", value => ShowTitle = (bool)value) { Prop = ShowTitle });
+            //Columnvisibility.Submenus.Add(new MyMenuItem("Title", value => ShowTitle = (bool)value) { Prop = ShowTitle });
             Columnvisibility.Submenus.Add(new MyMenuItem("Author", value => ShowAuthor = (bool)value) { Prop = ShowAuthor});
             Columnvisibility.Submenus.Add(new MyMenuItem("Publisher", value => ShowPublisher= (bool)value) { Prop = ShowPublisher });
             Columnvisibility.Submenus.Add(new MyMenuItem("Release Date", value => ShowReleaseDate = (bool)value) { Prop = ShowReleaseDate });
@@ -29,7 +29,7 @@ namespace COMPASS.ViewModels
             Columnvisibility.Submenus.Add(new MyMenuItem("Rating", value => ShowRating = (bool)value) { Prop = ShowRating });
             Columnvisibility.Submenus.Add(new MyMenuItem("Tags", value => ShowTags = (bool)value) { Prop = ShowTags });
             Columnvisibility.Submenus.Add(new MyMenuItem("File Icons", value => ShowFileIcons = (bool)value) { Prop = ShowFileIcons });
-            Columnvisibility.Submenus.Add(new MyMenuItem("Edit Icon", value => ShowEditIcon = (bool)value) { Prop = ShowEditIcon });
+            //Columnvisibility.Submenus.Add(new MyMenuItem("Edit Icon", value => ShowEditIcon = (bool)value) { Prop = ShowEditIcon });
             
             ViewOptions.Add(Columnvisibility);
         }
@@ -43,60 +43,76 @@ namespace COMPASS.ViewModels
             set{ SetProperty(ref _showTitle, value); }
         }
 
-        private bool _showAuthor = true;
+        private bool _showAuthor = Properties.Settings.Default.ListShowAuthor;
         public bool ShowAuthor
         {
             get { return _showAuthor; }
-            set { SetProperty(ref _showAuthor, value); }
+            set { SetProperty(ref _showAuthor, value);
+                Properties.Settings.Default.ListShowAuthor = value;
+            }
         }
 
-        private bool _showPublisher = false;
+        private bool _showPublisher = Properties.Settings.Default.ListShowPublisher;
         public bool ShowPublisher
         {
             get { return _showPublisher; }
-            set { SetProperty(ref _showPublisher, value); }
+            set { SetProperty(ref _showPublisher, value);
+                Properties.Settings.Default.ListShowPublisher = value;
+            }
         }
 
-        private bool _ShowReleaseDate = true;
+        private bool _ShowReleaseDate = Properties.Settings.Default.ListShowRelease;
         public bool ShowReleaseDate
         {
             get { return _ShowReleaseDate; }
-            set { SetProperty(ref _ShowReleaseDate, value); }
+            set { SetProperty(ref _ShowReleaseDate, value);
+                Properties.Settings.Default.ListShowRelease = value;
+            }
         }
 
-        private bool _showVersion = false;
+        private bool _showVersion = Properties.Settings.Default.ListShowVersion;
         public bool ShowVersion
         {
             get { return _showVersion; }
-            set { SetProperty(ref _showVersion, value); }
+            set { SetProperty(ref _showVersion, value);
+                Properties.Settings.Default.ListShowVersion = value;
+            }
         }
 
-        private bool _showRating = true;
+        private bool _showRating = Properties.Settings.Default.ListShowRating;
         public bool ShowRating
         {
             get { return _showRating; }
-            set { SetProperty(ref _showRating, value); }
+            set { SetProperty(ref _showRating, value);
+                Properties.Settings.Default.ListShowRating = value;
+            }
         }
 
-        private bool _showTags = true;
+        private bool _showTags = Properties.Settings.Default.ListShowTags;
         public bool ShowTags
         {
             get { return _showTags; }
-            set { SetProperty(ref _showTags, value); }
+            set { SetProperty(ref _showTags, value);
+                Properties.Settings.Default.ListShowTags = value;
+            }
         }
 
-        private bool _showFileIcons = true;
+        private bool _showFileIcons = Properties.Settings.Default.ListShowFileIcons;
         public bool ShowFileIcons
         {
             get { return _showFileIcons; }
-            set { SetProperty(ref _showFileIcons, value); }
+            set { SetProperty(ref _showFileIcons, value);
+                Properties.Settings.Default.ListShowFileIcons = value;
+            }
         }
 
-        private bool _showEditIcon = true;
+        private bool _showEditIcon = Properties.Settings.Default.ListShowEditIcon;
         public bool ShowEditIcon
         {
             get { return _showEditIcon; }
-            set { SetProperty(ref _showEditIcon, value); }
+            set { SetProperty(ref _showEditIcon, value);
+                Properties.Settings.Default.ListShowEditIcon = value;
+            }
         }
 
         #endregion
