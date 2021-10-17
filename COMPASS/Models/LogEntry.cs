5abbb9b6802b54f5523c6b8b3feed0848ababcae
DSPAUL <35136955+DSPAUL@users.noreply.github.com>
2021-10-17 18:59:42 +0200

    Added log to ProgressWindow

---
+++ COMPASS/Models/LogEntry.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMPASS.Models
+{
+    public class LogEntry : ObservableObject
+    {
+        public LogEntry(MsgType msgType, string message)
+        {
+            Type = msgType;
+            Msg = message;
+        }
+
+        private MsgType _Type;
+        public MsgType Type
+        {
+            get { return _Type; }
+            set { SetProperty(ref _Type, value); }
+        }
+
+        private string _msg;
+        public string Msg
+        {
+            get { return _msg; }
+            set { SetProperty(ref _msg, value); }
+        }
+
+        public enum MsgType
+        {
+            Info,
+            Warning,
+            Error
+        }
+    }
+}
