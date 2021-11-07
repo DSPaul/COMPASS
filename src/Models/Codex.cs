5fcfdf851f15abad30eff4eaaebdcb9aa08a1b39
DSPAUL <paul.de.smul@gmail.com>
2021-11-07 01:09:51 +0100

    Add Has(Off/On)lineSource() functions

--- src/Models/Codex.cs
+++ src/Models/Codex.cs
@@ -3,6 +3,7 @@ using COMPASS.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 namespace COMPASS.Models
@@ -43,92 +44,112 @@ namespace COMPASS.Models
             }
         }
 
+        public bool HasOfflineSource()
+        {
+            if (File.Exists(Path)) return true;
+            return false;
+        }
+
+        public bool HasOnlineSource()
+        {
+            if (SourceURL == null || SourceURL == "") return false;
+            return true;
+        }
+
+        #region Properties
+
         private string _Path;
-        private string _Title;
-        private string _Author;
-        private string _Publisher;
-        private string _Version;
-        private string _SourceURL;
-        private int _ID;
-        private string _CoverArt;
-        private bool _Physically_Owned;
-        private string _Description;
-        private DateTime? _ReleaseDate = null;
-        private int _Rating;
-        private int _PageCount;
-
-        private ObservableCollection<Tag> _Tags;
-
-        #region Getters and Setters
         public string Path
         {
             get { return _Path; }
             set { SetProperty(ref _Path, value); }
         }
+
+        private string _Title;
         public string Title
         {
             get { return _Title; }
             set { SetProperty(ref _Title, value); }
         }
+
+        private string _Author;
         public string Author
         {
             get { return _Author; }
             set { SetProperty(ref _Author, value); }
         }
+
+        private string _Publisher;
         public string Publisher
         {
             get { return _Publisher; }
             set { SetProperty(ref _Publisher, value); }
         }
+
+        private string _Version;
         public string Version
         {
             get { return _Version; }
             set { SetProperty(ref _Version, value); }
         }
+
+        private string _SourceURL;
         public string SourceURL
         {
             get { return _SourceURL; }
             set { SetProperty(ref _SourceURL, value); }
         }
+
+        private int _ID;
         public int ID
         {
             get { return _ID; }
             set { SetProperty(ref _ID, value); }
         }
+
+        private string _CoverArt;
         public string CoverArt
         {
             get { return _CoverArt; }
             set { SetProperty(ref _CoverArt, value); }
         }
+
+        private bool _Physically_Owned;
         public bool Physically_Owned
         {
             get { return _Physically_Owned; }
             set { SetProperty(ref _Physically_Owned, value); }
         }
+
+        private ObservableCollection<Tag> _Tags;
         public ObservableCollection<Tag> Tags
         {
             get { return _Tags; }
             set { SetProperty(ref _Tags, value); }
         }
 
+        private string _Description;
         public string Description
         {
             get { return _Description; }
             set { SetProperty(ref _Description, value); }
         }
 
+        private DateTime? _ReleaseDate = null;
         public DateTime? ReleaseDate
         {
             get { return _ReleaseDate; }
             set { SetProperty(ref _ReleaseDate, value); }
         }
 
+        private int _Rating;
         public int Rating
         {
             get { return _Rating; }
             set { SetProperty(ref _Rating, value); }
         }
 
+        private int _PageCount;
         public int PageCount
         {
             get { return _PageCount; }
