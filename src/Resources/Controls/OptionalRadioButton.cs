99469c65a834de3d5870f16cc78873830fdf876e
DSPAUL <paul.de.smul@gmail.com>
2021-11-09 12:37:23 +0100

    Create OptionalRadioButton.cs

---
+++ src/Resources/Controls/OptionalRadioButton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace COMPASS.Resources.Controls
+{
+    public class OptionalRadioButton : RadioButton
+    {
+        protected override void OnClick()
+        {
+            bool? wasChecked = this.IsChecked;
+            base.OnClick();
+            if (wasChecked == true)
+                this.IsChecked = false;
+        }
+    }
+}
