07e7cc7c743532d01a6a0fe9b82237df8d9923ff
DSPAUL <paul.de.smul@gmail.com>
2022-05-24 17:57:52 +0200

    Replaced Parent Selection with Drag and Drop

--- src/ViewModels/TagEditViewModel.cs
+++ src/ViewModels/TagEditViewModel.cs
@@ -15,14 +15,10 @@ namespace COMPASS.ViewModels
             EditedTag = ToEdit;
             TempTag = new Tag(vm.CurrentCollection.AllTags);
             if (!CreateNewTag) TempTag.Copy(EditedTag);
-            else ClearParent();
 
             ShowColorSelection = false;
-            ShowParentSelection = false;
 
             //Commands
-            ClearParentCommand = new BasicCommand(ClearParent);
-            CancelParentSelectionCommand = new BasicCommand(CancelParentSelection);
             CloseColorSelectionCommand = new BasicCommand(CloseColorSelection);
         }
 
@@ -42,29 +38,6 @@ namespace COMPASS.ViewModels
             set { SetProperty(ref tempTag, value); }
         }
 
-        //Parent of tempTag for binding
-        public Tag ParentTempTag
-        {
-            get { return TempTag.GetParent(); }
-            set 
-            { 
-                TempTag.ParentID = value.ID;
-                RaisePropertyChanged();
-            }
-        }
-
-        //visibility of parenttree
-        private bool showparentselection = false;
-        public bool ShowParentSelection
-        {
-            get { return showparentselection; }
-            set 
-            {                
-                SetProperty(ref showparentselection, value);
-                RaisePropertyChanged("ShowInfoGrid");
-            }
-        }
-
         //visibility of Color Selection
         private bool showcolorselection = false;
         public bool ShowColorSelection
@@ -80,89 +53,33 @@ namespace COMPASS.ViewModels
         //visibility of General Info Selection
         public bool ShowInfoGrid
         {
-            get { return !(ShowParentSelection || ShowColorSelection); }
+            get { return !ShowColorSelection; }
             set { }
-        }
-
-        //Selected Parent from Treeview
-        public Tag SelectedTag
-        {
-            get
-            {
-                foreach (TreeViewNode t in AllTreeViewNodes)
-                {
-                    if (t.Selected) return t.Tag;
-                }
-                return null;
-            }
-            set
-            {
-                if (value == null)
-                {
-                    TempTag.ParentID = -1;
-                    foreach (TreeViewNode t in AllTreeViewNodes)
-                    {
-                        t.Selected = false;
-                    }
-                }
-
-                else
-                {
-                    foreach (TreeViewNode t in AllTreeViewNodes)
-                    {
-                        if (t.Tag == value)
-                        {
-                            t.Selected = true;
-                            //Set Parent tag when different tag is selected
-                            ParentTempTag = t.Tag;
-                            ShowParentSelection = false;
-                        }
-                    }
-                }
-                
-            }
         }
 
         #endregion
 
         #region Functions and Commands
 
-        //Refreshes all the files to apply changes in Tag
-        public BasicCommand ClearParentCommand { get; private set; }
-        private void ClearParent()
-        {
-            SelectedTag = null;
-            RaisePropertyChanged("ParentTempTag");
-        }
-
         public override void OKBtn()
         {
-            bool CreatingTag = false;
             if(CreateNewTag)
             {
                 EditedTag = new Tag(CurrentCollection.AllTags);
-                CreatingTag = true;
                 if (TempTag.ParentID == -1) CurrentCollection.RootTags.Add(EditedTag);
             }
-            //set Parent if changed
-            if (EditedTag.ParentID != tempTag.ParentID)
-            {
-                if (EditedTag.ParentID == -1) CurrentCollection.RootTags.Remove(EditedTag);
-                else EditedTag.GetParent().Items.Remove(tempTag);
 
-                if (TempTag.ParentID == -1) CurrentCollection.RootTags.Add(EditedTag);
-                else TempTag.GetParent().Items.Add(EditedTag);
-            }
             //Apply changes 
             EditedTag.Copy(TempTag);
             MVM.TFViewModel.RefreshTreeView();
-            if (!CreatingTag) CloseAction();
+
+            if (!CreateNewTag) CloseAction();
             else
             {
                 CurrentCollection.AllTags.Add(EditedTag);
+                //reset fields
                 TempTag = new Tag(CurrentCollection.AllTags);
                 EditedTag = null;
-                RaisePropertyChanged("ParentTempTag");
             }
         }
 
@@ -174,12 +91,6 @@ namespace COMPASS.ViewModels
                 TempTag = new Tag(CurrentCollection.AllTags);
             }
             EditedTag = null;
-        }
-
-        public BasicCommand CancelParentSelectionCommand { get; private set;}
-        private void CancelParentSelection()
-        {
-            ShowParentSelection = false;
         }
 
         public BasicCommand CloseColorSelectionCommand { get; private set; }
