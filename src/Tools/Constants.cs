fc4b0a1ed3df8e3615d6c580f51a3a05f1e1af95
DSPAUL <paul.de.smul@gmail.com>
2022-06-15 16:54:44 +0200

    fix path

--- src/Tools/Constants.cs
+++ src/Tools/Constants.cs
@@ -11,6 +11,6 @@ namespace COMPASS.Tools
     {
         public static readonly string RepoURL = "https://github.com/DSPAUL/COMPASS";
         public static readonly string CompassDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),"COMPASS");
-        public static readonly string PreferencesFilePath = Path.Combine(CompassDataPath + "Preferences.xml");
+        public static readonly string PreferencesFilePath = Path.Combine(CompassDataPath + "\\Preferences.xml");
     }
 }
