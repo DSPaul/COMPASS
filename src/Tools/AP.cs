27dbec6c460ab33ec73e76e5adc13e51a6f94d70
DSPAUL <paul.de.smul@gmail.com>
2022-06-16 11:22:07 +0200

    Add UI for renaming folder

--- src/Tools/AP.cs
+++ src/Tools/AP.cs
@@ -22,5 +22,20 @@ namespace COMPASS.Tools
         public static readonly DependencyProperty IconKindProperty =
             DependencyProperty.RegisterAttached("IconKind", typeof(PackIconKind), typeof(AP));
         #endregion
+
+        #region PlaceHolderText
+        public static string GetPlaceHolderText(DependencyObject obj)
+        {
+            return (string)obj.GetValue(PlaceHolderTextProperty);
+        }
+
+        public static void SetPlaceHolderText(DependencyObject obj, string value)
+        {
+            obj.SetValue(PlaceHolderTextProperty, value);
+        }
+
+        public static readonly DependencyProperty PlaceHolderTextProperty =
+            DependencyProperty.RegisterAttached("PlaceHolderText", typeof(string), typeof(AP));
+        #endregion
     }
 }
