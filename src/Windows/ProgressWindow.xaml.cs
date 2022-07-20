9743679584dd57e223653eb466cd154c43630dad
DSPAUL <35136955+DSPAUL@users.noreply.github.com>
2022-07-20 17:03:35 +0200

    Add option to close progressWindow if import fails

--- src/Windows/ProgressWindow.xaml.cs
+++ src/Windows/ProgressWindow.xaml.cs
@@ -27,8 +27,12 @@ namespace COMPASS.Windows
 
         private void ProgressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            if (ProgBar.Value == 100) this.Close();
+            if (ProgBar.Value >= 100) this.Close();
         }
 
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
