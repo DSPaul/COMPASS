676fe393f56bc975ca3ff3f670aeb47f0141f4cd
DSPAUL <35136955+DSPAUL@users.noreply.github.com>
2022-07-22 23:25:54 +0200

    Move Constants to Models

---
+++ src/Models/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace COMPASS.Models
+{
+    public static class Constants
+    {
+        public const string RepoURL = "https://github.com/DSPAUL/COMPASS";
+        public static readonly string CompassDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "COMPASS");
+        public static readonly string PreferencesFilePath = Path.Combine(CompassDataPath,"Preferences.xml");
+        public static readonly string WebDriverDirectoryPath = Path.Combine(CompassDataPath,"WebDrivers");
+        public static readonly string InstallersPath = Path.Combine(CompassDataPath,"Installers");
+    }
+}
