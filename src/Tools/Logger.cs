dbaa3c13f3af7d999e64cad911cf2ffaff73db58
DSPAUL <paul.de.smul@gmail.com>
2023-02-02 19:05:07 +0100

    Fix empty log messages

--- src/Tools/Logger.cs
+++ src/Tools/Logger.cs
@@ -10,8 +10,7 @@ namespace COMPASS.Tools
         public static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         public static void LogUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
         {
-            string errorMessage = e.Exception.Message;
-            errorMessage = e.Exception.InnerException?.ToString();
+            string errorMessage = (e.Exception.InnerException is null) ? e.Exception.Message : e.Exception.InnerException.ToString();
             log.Fatal(errorMessage);
             //prompt user to submit logs and open an issue
             string message = "Its seems COMPASS has run into an error and crashed.\n" +
