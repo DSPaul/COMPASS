a76eff4274c5066aa904113de0f39d2269aa70b1
DSPAUL <paul.de.smul@gmail.com>
2023-02-11 16:31:09 +0100

    Generalize Returning relay command

--- src/Commands/ReturningRelayCommand.cs
+++ src/Commands/ReturningRelayCommand.cs
@@ -5,9 +5,14 @@ namespace COMPASS.Commands
 {
     //Relaycommand that works with functions that return bool
     //to for example indicate succes of excecution
-    public class ReturningRelayCommand<T> : ICommand
+    /// <summary>
+    /// Command for methods that take one argument and return bool that indicates success
+    /// </summary>
+    /// <typeparam name="T"> Type of function argument </typeparam>
+    /// <typeparam name="R"> Type of return value </typeparam>
+    public class ReturningRelayCommand<T,R> : ICommand
     {
-        private readonly Func<T, bool> _execute;
+        private readonly Func<T, R> _execute;
         private readonly Func<T, bool> _canExecute;
 
         public event EventHandler CanExecuteChanged
@@ -16,7 +21,7 @@ namespace COMPASS.Commands
             remove => CommandManager.RequerySuggested -= value;
         }
 
-        public ReturningRelayCommand(Func<T, bool> Execute, Func<T, bool> CanExecute = null)
+        public ReturningRelayCommand(Func<T, R> Execute, Func<T, bool> CanExecute = null)
         {
             _execute = Execute;
             _canExecute = CanExecute;
