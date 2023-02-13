efc0232e0c274b5967c3d9eb4277e1c859ba8b11
DSPAUL <paul.de.smul@gmail.com>
2023-02-13 15:10:49 +0100

    Create ExtensionMethods.cs

---
+++ src/Tools/ExtensionMethods.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace COMPASS.Tools
+{
+    public static class ExtensionMethods
+    {
+        #region ObservableCollection Extensions
+        /// <summary>
+        /// Remove All Items in place that match the condition.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection"></param>
+        /// <param name="condition"></param>
+        public static void RemoveAll<T>(
+        this ObservableCollection<T> collection, Func<T, bool> condition)
+        {
+            var itemsToRemove = collection.Where(condition);
+            if (itemsToRemove.Count() == collection.Count)
+            {
+                collection.Clear();
+            }
+
+            else
+            {
+                foreach (var itemToRemove in itemsToRemove)
+                {
+                    collection.Remove(itemToRemove);
+                }
+            }
+        }
+        #endregion
+    }
+}
