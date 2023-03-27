98aea943f68ceb6202d8d6d7d83efef600d09a7f
DSPAUL <paul.de.smul@gmail.com>
2023-03-27 23:17:10 +0200

    Keep Compatibility with old class name

--- src/Models/GlobalPreferences.cs
+++ src/Models/GlobalPreferences.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Xml.Serialization;
 
 namespace COMPASS.Models
 {
@@ -6,6 +7,7 @@ namespace COMPASS.Models
     /// <summary>
     /// Class that contains all the global preferences, ready to be serialized
     /// </summary>
+    [XmlRoot("SerializablePreferences")]
     public class GlobalPreferences
     {
         public List<int> OpenFilePriorityIDs { get; set; }
