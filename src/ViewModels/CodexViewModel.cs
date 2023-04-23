a1d91ba291136953e08dd2606b20a42f37fe775f
DSPAUL <paul.de.smul@gmail.com>
2023-04-23 20:32:32 +0200

    Fix crash

--- src/ViewModels/CodexViewModel.cs
+++ src/ViewModels/CodexViewModel.cs
@@ -94,8 +94,8 @@ namespace COMPASS.ViewModels
         }
 
         //Open Multiple Files
-        private ReturningRelayCommand<IEnumerable<Codex>, bool> _openSelectedCodicesCommand;
-        public ReturningRelayCommand<IEnumerable<Codex>, bool> OpenSelectedCodicesCommand => _openSelectedCodicesCommand ??= new(OpenSelectedCodices);
+        private ReturningRelayCommand<IList, bool> _openSelectedCodicesCommand;
+        public ReturningRelayCommand<IList, bool> OpenSelectedCodicesCommand => _openSelectedCodicesCommand ??= new(l => OpenSelectedCodices(l.Cast<Codex>()));
         public static bool OpenSelectedCodices(IEnumerable<Codex> toOpen)
         {
             List<Codex> ToOpen = toOpen?.ToList();
