f4645b97a1f44d2ec2143dde0f657f7c77533f81
DSPAUL <paul.de.smul@gmail.com>
2024-02-09 22:25:35 +0100

    Write first actual test

---
+++ COMPASS_Tests/Sources/HomeBrewery.cs
@@ -0,0 +1,39 @@
+﻿using COMPASS.Models;
+using COMPASS.ViewModels;
+using COMPASS.ViewModels.Sources;
+
+namespace COMPASS_Tests.Services
+{
+    [TestClass]
+    public class HomeBrewery
+    {
+        static MainViewModel? mvm;
+
+        [ClassInitialize]
+        public static void Init(TestContext testContext)
+        {
+            if (System.Windows.Application.Current == null)
+                new System.Windows.Application();
+
+            mvm = new MainViewModel();
+            mvm.
+        }
+
+        [TestMethod]
+        public async Task GetMetaDataFromHomeBrewery()
+        {
+            Codex codex = new()
+            {
+                SourceURL = @"https://homebrewery.naturalcrit.com/share/FegJIEB2KUUo"
+            };
+
+            var vm = SourceViewModel.GetSourceVM(MetaDataSource.Homebrewery);
+
+            Codex response = await vm.GetMetaData(codex);
+
+            Assert.IsNotNull(response);
+            Assert.IsFalse(String.IsNullOrEmpty(response.Title));
+            Assert.IsFalse(String.IsNullOrEmpty(response.Description));
+        }
+    }
+}
--- COMPASS_Tests/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-namespace COMPASS_Tests
-{
-    [TestClass]
-    public class UnitTest1
-    {
-        [TestMethod]
-        public void TestMethod1() => Assert.IsTrue(true);
-    }
-}
