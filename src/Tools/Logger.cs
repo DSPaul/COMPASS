d3e7e26d4d22249c3ca72c613a6f65f2d213ab3e
DSPAUL <paul.de.smul@gmail.com>
2024-05-31 16:43:46 +0200

    Add automatic crash log submission

--- src/Tools/Logger.cs
+++ src/Tools/Logger.cs
@@ -1,10 +1,13 @@
 ﻿using Autofac;
 using COMPASS.Interfaces;
 using COMPASS.Models;
+using COMPASS.Models.ApiDtos;
 using COMPASS.Models.Enums;
+using COMPASS.Services;
 using COMPASS.ViewModels;
 using System;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.IO;
 using System.Runtime.ExceptionServices;
 using System.Windows;
@@ -62,13 +65,31 @@ namespace COMPASS.Tools
             FileLog?.Fatal(e.Exception.ToString(), e.Exception);
 
             //prompt user to submit logs and open an issue
-            string message = $"Its seems COMPASS has run into a critical error ({e.Exception.Message}).\n" +
+            string message = $"An unexpected error ocurred.\n" +
+                $"{e.Exception.Message}" +
                 $"You can help improve COMPASS by opening an issue on {Constants.RepoURL} with the error message. \n" +
-                $"Please include the log file located at {SettingsViewModel.CompassDataPath}\\logs.";
+                $"Please include the log file located at {SettingsViewModel.CompassDataPath}\\logs \n." +
+                $"COMPASS will now restart.";
+
             Notification crashNotification = new($"COMPASS ran into a critical error.", message, Severity.Error);
+            string submitOption = "Submit an anonymous crash report.";
+            crashNotification.Options.Add(new(submitOption, true));
+
             App.Container.ResolveKeyed<INotificationService>(NotificationDisplayType.Windowed).Show(crashNotification);
 
+            if (crashNotification.IsOptionSelected(submitOption))
+            {
+                var report = new CrashReport(e.Exception);
+                var result = new ApiClientService().PostAsync(report, "submit/crash").Result;
+                Info(result.ToString());
+            }
+
             e.Handled = true;
+
+            //Restart
+            var currentExecutablePath = Environment.ProcessPath;
+            var args = Environment.GetCommandLineArgs();
+            if (currentExecutablePath != null) Process.Start(currentExecutablePath, args);
             Environment.Exit(1);
         }
 
