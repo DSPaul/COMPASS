e9fc13b2029ba292b3e07efe13450ef4b44f63bd
Paul De Smul <paul.de.smul@gmail.com>
2024-11-07 20:57:42 +0100

    Finish tile layout except context menu

---
+++ Source/COMPASS.Common/Converters/FuncConverters.cs
@@ -0,0 +1,12 @@
+﻿using Avalonia.Data.Converters;
+
+namespace COMPASS.Common.Converters
+{
+    public static class FuncConverters
+    {
+        public static FuncValueConverter<object?, object?, bool> AreEqual { get; } =
+           new FuncValueConverter<object?, object?, bool>(areEqual);
+
+        private static bool areEqual(object? value, object? param) => value?.Equals(param) ?? false;
+    }
+}
