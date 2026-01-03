7a46218611df2223cd04c82df743308f03453b47
Paul De Smul <paul.de.smul@gmail.com>
2026-01-03 16:10:40 +0100

    Add avalonia accelerate debugging tools

--- Source/COMPASS.Common/App.axaml.cs
+++ Source/COMPASS.Common/App.axaml.cs
@@ -14,7 +14,14 @@ namespace COMPASS.Common;
 
 public partial class App : Application
 {
-    public override void Initialize() => AvaloniaXamlLoader.Load(this);
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+        
+#if DEBUG
+        this.AttachDeveloperTools();
+#endif
+    }
 
     public override void OnFrameworkInitializationCompleted()
     {
