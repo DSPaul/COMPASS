fec2743e60a8e2dd88694e5432c901138155d196
Paul De Smul <paul.de.smul@gmail.com>
2026-02-26 20:44:27 +0100

    Port Card Layout

--- Source/COMPASS.Common/Views/Layouts/CardLayout.axaml.cs
+++ Source/COMPASS.Common/Views/Layouts/CardLayout.axaml.cs
@@ -1,3 +1,9 @@
+using Avalonia.Controls;
+using Avalonia.Input;
+using COMPASS.Common.Operations;
+using COMPASS.Common.ViewModels.Main;
+using COMPASS.Common.ViewModels.ModelVMs;
+
 namespace COMPASS.Common.Views.Layouts;
 
 public partial class CardLayout : CodexLayoutView
@@ -5,5 +11,19 @@ public partial class CardLayout : CodexLayoutView
     public CardLayout()
     {
         InitializeComponent();
+    }
+
+    private void ListBox_KeyDown(object? sender, KeyEventArgs e)
+    {
+        var operations = TabsViewModel.GetInstance().ActiveTab!.CodexCommands;
+        operations.HandleKeyDownOnCodex((sender as ListBox)?.SelectedItems, e);
+    }
+
+    private async void Codex_DoubleTapped(object? sender, TappedEventArgs e)
+    {
+        if (sender is Border { DataContext: CodexViewModel codexVm })
+        {
+            await CodexOperations.OpenCodex(codexVm.GetModel());
+        }
     }
 }
