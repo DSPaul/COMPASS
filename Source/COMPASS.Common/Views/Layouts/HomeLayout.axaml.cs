2e6ea0108aa0681b41f03babb0b71ff5e2fc2f7e
Paul De Smul <paul.de.smul@gmail.com>
2026-03-06 17:34:38 +0100

    Port Home Layout

--- Source/COMPASS.Common/Views/Layouts/HomeLayout.axaml.cs
+++ Source/COMPASS.Common/Views/Layouts/HomeLayout.axaml.cs
@@ -1,3 +1,9 @@
+using Avalonia.Controls;
+using Avalonia.Input;
+using COMPASS.Common.Operations;
+using COMPASS.Common.ViewModels.Main;
+using COMPASS.Common.ViewModels.ModelVMs;
+
 namespace COMPASS.Common.Views.Layouts;
 
 public partial class HomeLayout : CodexLayoutView
@@ -5,5 +11,19 @@ public partial class HomeLayout : CodexLayoutView
     public HomeLayout()
     {
         InitializeComponent();
+    }
+
+    private void HandleKeyDown(object? sender, KeyEventArgs e)
+    {
+        var operations = TabsViewModel.GetInstance().ActiveTab!.CodexCommands;
+        operations.HandleKeyDownOnCodex((sender as ListBox)?.SelectedItems, e);
+    }
+
+    private async void Codex_DoubleTapped(object? sender, TappedEventArgs e)
+    {
+        if (sender is Grid { DataContext: CodexViewModel codexVm })
+        {
+            await CodexOperations.OpenCodex(codexVm.GetModel());
+        }
     }
 }
