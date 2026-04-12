0b1494cd29c09290672871e4caac8402790e4db3
Paul De Smul <paul.de.smul@gmail.com>
2026-04-12 11:00:43 +0200

    Improve selection highlight

---
+++ Source/COMPASS.Common/Controls/RoundedImage.cs
@@ -0,0 +1,35 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+namespace COMPASS.Common.Controls
+{
+    public class RoundedImage : Image
+    {
+        public static readonly AttachedProperty<double> CornerRadiusProperty =
+            AvaloniaProperty.RegisterAttached<RoundedImage, double>(
+                "CornerRadius", typeof(RoundedImage), 5);
+
+        public static void SetCornerRadius(AvaloniaObject element, double parameter)
+        {
+            element.SetValue(CornerRadiusProperty, parameter);
+        }
+
+        public static double GetCornerRadius(AvaloniaObject element)
+        {
+            return element.GetValue(CornerRadiusProperty);
+        }
+
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            IImage? source = Source;
+            Size result = new();
+
+            if (source != null)
+            {
+                result = Stretch.CalculateSize(availableSize, source.Size, StretchDirection);
+            }
+            Clip = new RectangleGeometry(new Rect(0, 0, result.Width, result.Height), GetCornerRadius(this), GetCornerRadius(this));
+            return result;
+        }
+    }
+}
