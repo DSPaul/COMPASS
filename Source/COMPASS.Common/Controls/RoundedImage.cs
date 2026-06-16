596e71ce06a959c690054dd4992c82d466fa1eab
Paul De Smul <paul.de.smul@gmail.com>
2026-06-17 00:01:09 +0200

    Fix rounded image

--- Source/COMPASS.Common/Controls/RoundedImage.cs
+++ Source/COMPASS.Common/Controls/RoundedImage.cs
@@ -9,27 +9,75 @@ namespace COMPASS.Common.Controls
             AvaloniaProperty.RegisterAttached<RoundedImage, CornerRadius>(
                 "CornerRadius", typeof(RoundedImage), new CornerRadius(5));
 
-        public static void SetCornerRadius(AvaloniaObject element, CornerRadius parameter)
+        public CornerRadius CornerRadius
         {
-            element.SetValue(CornerRadiusProperty, parameter);
-        }
-
-        public static CornerRadius GetCornerRadius(AvaloniaObject element)
-        {
-            return element.GetValue(CornerRadiusProperty);
+            get => GetValue(CornerRadiusProperty);
+            set => SetValue(CornerRadiusProperty, value);
         }
 
         protected override Size MeasureOverride(Size availableSize)
         {
             IImage? source = Source;
-            Size result = new();
+            Size bounds = new();
 
             if (source != null)
             {
-                result = Stretch.CalculateSize(availableSize, source.Size, StretchDirection);
+                bounds = Stretch.CalculateSize(availableSize, source.Size, StretchDirection);
             }
-            Clip = new RectangleGeometry(new Rect(0, 0, result.Width, result.Height), GetCornerRadius(this).TopLeft, GetCornerRadius(this).TopRight);
-            return result;
+
+            if(CornerRadius == default)
+            {
+                Clip = new RectangleGeometry(new Rect(bounds));
+            }
+            else if (CornerRadius.IsUniform)
+            {
+                Clip = new RectangleGeometry(new Rect(bounds), radiusX: CornerRadius.TopLeft, radiusY: CornerRadius.TopRight);
+            }
+            else
+            {
+                var mask = new PathGeometry();
+                Point start = new Point(CornerRadius.TopLeft, 0);
+                mask.Figures!.Add(new PathFigure
+                {
+                    StartPoint = start,
+                    Segments = new PathSegments
+                    {
+                        new LineSegment { Point = new Point(bounds.Width - CornerRadius.TopRight, 0) },
+                        new ArcSegment
+                        {
+                            Point = new Point(bounds.Width, CornerRadius.TopRight),
+                            Size = new Size(CornerRadius.TopRight, CornerRadius.TopRight),
+                            SweepDirection = SweepDirection.Clockwise
+                        },
+                        new LineSegment { Point = new Point(bounds.Width, bounds.Height - CornerRadius.BottomRight)},
+                        new ArcSegment
+                        {
+                            Point = new Point(bounds.Width - CornerRadius.BottomRight, bounds.Height),
+                            Size = new Size(CornerRadius.BottomRight, CornerRadius.BottomRight), 
+                            SweepDirection = SweepDirection.Clockwise
+                        },
+                        new LineSegment { Point = new Point(CornerRadius.BottomLeft, bounds.Height) },
+                        new ArcSegment
+                        {
+                            Point = new Point(0, bounds.Height - CornerRadius.BottomLeft),
+                            Size = new Size(CornerRadius.BottomLeft, CornerRadius.BottomLeft),
+                            SweepDirection = SweepDirection.Clockwise
+                        },
+                        new LineSegment { Point = new Point(0, CornerRadius.TopLeft)},
+                        new ArcSegment
+                        {
+                            Point = start,
+                            Size = new Size(CornerRadius.TopLeft, CornerRadius.TopLeft),
+                            SweepDirection = SweepDirection.Clockwise,
+                        },
+                    },
+                    IsClosed = true
+                });
+
+                Clip = mask;
+            }
+
+            return bounds;
         }
     }
 }
