2234c296b05c0429cb657dee378472c34e0e096a
Paul De Smul <paul.de.smul@gmail.com>
2026-08-09 19:23:43 +0200

    Make tags 70% transparent

--- Source/COMPASS.Infra.Avalonia/Converters/ColorConverters.cs
+++ Source/COMPASS.Infra.Avalonia/Converters/ColorConverters.cs
@@ -9,7 +9,28 @@ namespace COMPASS.Infra.Avalonia.Converters
         public static FuncValueConverter<Color, SolidColorBrush> ColorToBrush { get; } = 
             new (color => new SolidColorBrush(color));
 
-        public static FuncValueConverter<SolidColorBrush, string, SolidColorBrush> AddTransparency { get; } =
-            new((brush, transparency) => new SolidColorBrush(Color.FromArgb((byte)(brush!.Color.A * float.Parse(transparency ?? "1", CultureInfo.InvariantCulture)), brush.Color.R, brush.Color.G, brush.Color.B)));
+        public static FuncValueConverter<SolidColorBrush?, string, SolidColorBrush?> AddTransparency { get; } =
+            new((brush, transparency) =>
+            {
+                if (brush is null)
+                {
+                    return brush;
+                }
+                byte alpha = (byte)(brush.Color.A * float.Parse(transparency ?? "1", CultureInfo.InvariantCulture));
+                return new SolidColorBrush(Color.FromArgb(alpha, brush.Color.R, brush.Color.G, brush.Color.B));
+            });
+
+        public static FuncValueConverter<Color?, string, SolidColorBrush?> ColorToTransparentBrush { get; } =
+            new((color, transparency) =>
+            {
+                if(color is null)
+                {
+                    return null;
+                }
+
+                var brush = new SolidColorBrush(color.Value);
+                byte alpha = (byte)(brush.Color.A * float.Parse(transparency ?? "1", CultureInfo.InvariantCulture));
+                return new SolidColorBrush(Color.FromArgb(alpha, brush.Color.R, brush.Color.G, brush.Color.B));
+            });
     }
 }
