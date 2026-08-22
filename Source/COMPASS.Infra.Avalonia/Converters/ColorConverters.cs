19cee57a652cd34cda17098ca7752f66cdc6bcad
Paul De Smul <paul.de.smul@gmail.com>
2026-08-22 17:18:16 +0200

    Fix transparency converter

--- Source/COMPASS.Infra.Avalonia/Converters/ColorConverters.cs
+++ Source/COMPASS.Infra.Avalonia/Converters/ColorConverters.cs
@@ -9,15 +9,24 @@ namespace COMPASS.Infra.Avalonia.Converters
         public static FuncValueConverter<Color, SolidColorBrush> ColorToBrush { get; } = 
             new (color => new SolidColorBrush(color));
 
-        public static FuncValueConverter<SolidColorBrush?, string, SolidColorBrush?> AddTransparency { get; } =
+        public static FuncValueConverter<IBrush?, string, SolidColorBrush?> AddTransparency { get; } =
             new((brush, transparency) =>
             {
                 if (brush is null)
                 {
-                    return brush;
+                    return null;
                 }
-                byte alpha = (byte)(brush.Color.A * float.Parse(transparency ?? "1", CultureInfo.InvariantCulture));
-                return new SolidColorBrush(Color.FromArgb(alpha, brush.Color.R, brush.Color.G, brush.Color.B));
+
+                float addedTransparencey = float.Parse(transparency ?? "1", CultureInfo.InvariantCulture);
+
+                switch (brush)
+                {
+                    case ISolidColorBrush solidBrush:
+                        byte alpha = (byte)(solidBrush.Color.A * addedTransparencey);
+                        return new SolidColorBrush(Color.FromArgb(alpha, solidBrush.Color.R, solidBrush.Color.G, solidBrush.Color.B));
+                }
+
+                return null;
             });
 
         public static FuncValueConverter<Color?, string, SolidColorBrush?> ColorToTransparentBrush { get; } =
